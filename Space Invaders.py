f79a2b3335edfc91a1bd5817902af269c59afd7c
TeddyHDYT <elijah-brauch@web.de>
2025-03-07 11:06:27 +0100

    made player movement window size independent
    added title and icon

--- Space Invaders.py
+++ Space Invaders.py
@@ -6,20 +6,24 @@
 import pygame
 import math
 
+# Load images
+player = pygame.image.load("textures/player.png")
+
 # Initialize pygame
 pygame.init()
 screen = pygame.display.set_mode((400, 300), pygame.SCALED)
+pygame.display.set_caption("Space Invaders")
+pygame.display.set_icon(player)
 clock = pygame.time.Clock()
 running = True
 
 # Variables
-player_xy = (200,200)
-player_speed = 5
+player_xy = (screen.get_width() / 2, screen.get_height() - 100)
+player_speed = 3
 player_missile_list = []
-player_missile_speed = 10
-
-# Player
-player = pygame.image.load("textures/player.png")
+player_missile_speed = 15
+player_missile_cooldown = 5 # in frames
+cooldown = 0
 
 # Main Loop
 while running:
@@ -31,8 +35,9 @@ while running:
     keys = pygame.key.get_pressed()
     x = keys[pygame.K_d] - keys[pygame.K_a]
     y = keys[pygame.K_s] - keys[pygame.K_w]
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_SPACE] and cooldown == 0:
         player_missile_list.append((player_xy[0] + 7, player_xy[1], True))
+        cooldown = player_missile_cooldown
     if keys[pygame.K_ESCAPE]:
         running = False
 
@@ -42,6 +47,12 @@ while running:
         player_missile_list[i] = (player_missile_list[i][0], player_missile_list[i][1] - player_missile_speed, True)
         if player_missile_list[i][1] < -10:
             player_missile_list[i] = (-10, -10, False)
+        
+    # Player missile cooldown
+    if cooldown > 0:
+        cooldown -= 1
+
+    
     # Player missile cleanup
     for i in range(len(player_missile_list)):
         i -= 1
@@ -60,17 +71,17 @@ while running:
     # Player bounds check
     if player_xy[0] < -2:
         player_xy = (-2, player_xy[1])
-    if player_xy[0] > 385:
-        player_xy = (385, player_xy[1])
+    if player_xy[0] > screen.get_width() - 15:
+        player_xy = (screen.get_width() - 15, player_xy[1])
     if player_xy[1] < 0:
         player_xy = (player_xy[0], 0)
-    if player_xy[1] > 285:
-        player_xy = (player_xy[0], 285)
+    if player_xy[1] > screen.get_height() - 15:
+        player_xy = (player_xy[0], screen.get_height() - 15)
 
     # Draw screen
     screen.fill((10, 10, 15))
     for i in range(len(player_missile_list)):
-        pygame.draw.rect(screen, (255, 0, 0), (player_missile_list[i][0], player_missile_list[i][1], 2, 5))
+        pygame.draw.rect(screen, (255, 0, 0), (player_missile_list[i][0], player_missile_list[i][1], 2, 4))
     screen.blit(player, player_xy)
     pygame.display.flip()
     clock.tick(30)
